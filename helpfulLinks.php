b1dc66e1b530f935c97924448487063fa85a9ee8
Nicolas Rannou <huko.rannou@gmail.com>
2013-10-29 14:12:50 -0400

    add google analytics: admin@babymri.org -> rebekah.mannix

--- helpfulLinks.php
+++ helpfulLinks.php
@@ -1,6 +1,8 @@
 <?php
 $content = file_get_contents("templates/helpfulLinks.html");
 
+// add analytics
+$content = str_replace('${ANALYTICS}', file_get_contents("analyticstracking.php"), $content);
 // add footer
 $content = str_replace('${FOOTER}', file_get_contents("templates/footer.html"), $content);
 
--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 $content = file_get_contents("templates/home.html");
 
+// add analytics
+$content = str_replace('${ANALYTICS}', file_get_contents("analyticstracking.php"), $content);
 // add footer
 $content = str_replace('${FOOTER}', file_get_contents("templates/footer.html"), $content);
 
